2dcbd0d3379bb03e437339c6272e0087554ac7a7
Mak Shimizu <mak@fish.razor.jp>
2020-09-03 18:26:39 +0900

    remove unnecessary images and add ingestly page code

---
+++ static/img/ingestly-page-ms.js
@@ -0,0 +1,70 @@
+(function () {
+    // Cut the mustard
+    if ('querySelector' in window.top.document &&
+        'addEventListener' in window.top
+    ) {
+        Ingestly.config({
+            endpoint: 'f.makoto-shimizu.com',
+            apiKey: 'yay',
+            eventName: 'ingestlyRecurringEvent',
+            eventFrequency: 250,
+            targetWindow: 'self',
+            useCookie: true,
+            options: {
+                rum: {
+                    enable: true
+                },
+                unload: {
+                    enable: true
+                },
+                clicks: {
+                    enable: true,
+                    targetAttr: 'data-trackable'
+                },
+                scroll: {
+                    enable: true,
+                    threshold: 2,
+                    granularity: 20,
+                    unit: 'percent'
+                },
+                read: {
+                    enable: true,
+                    threshold: 2,
+                    granularity: 20,
+                    targets: window.document.getElementsByTagName('article')
+                },
+                media: {
+                    enable: true,
+                    heartbeat: 5
+                },
+                form: {
+                    enable: true,
+                    targets: window.document.getElementsByTagName('form')
+                }
+            }
+        });
+
+        Ingestly.init({
+            pgUrl: window.location.href,
+            pgRef: document.referrer,
+            pgTitle: window.document.title,
+            pgAttr: {},
+            usId: '',
+            usStatus: '',
+            usAttr: {},
+            cnId: '',
+            cnHeadline: '',
+            cnStatus: '',
+            cnAttr: {},
+            cpCode: Ingestly.getQueryVal('cid'),
+            cpName: Ingestly.getQueryVal('utm_campaign'),
+            cpSource: Ingestly.getQueryVal('utm_source'),
+            cpMedium: Ingestly.getQueryVal('utm_medium'),
+            cpTerm: Ingestly.getQueryVal('utm_term'),
+            cpContent: Ingestly.getQueryVal('utm_content'),
+            cpAttr: {}
+        });
+
+        Ingestly.trackPage();
+    }
+}());
